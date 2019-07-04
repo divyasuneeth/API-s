21c502adcecb90143205b58d659c34eea20ef402
divyasuneeth <shinkynambiar@gmail.com>
2019-07-04 07:15:23 +0530

    Update geocode.py

--- geocode.py
+++ geocode.py
@@ -2,7 +2,7 @@ import httplib2 # http library in python
 import json # convert in memory python objects to serialized json objects
 
 def getGeocodeLocation(inputString):
-    google_api_key="AIzaSyDojugkn3ZOsQ54Md2f68sAgDrnfzgsRg8"
+    google_api_key=""
     locationString=inputString.replace("","+")
     url=('https://maps.googleapis.com/maps/api/geocode/json?address=%s&key=%s'%(locationString,google_api_key))
     h = httplib2.Http()
